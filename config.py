69b344a5920c33eaabc2c5d33a26ecf525f23c5c
RNGDebeloper <131392755+RNGDebeloper@users.noreply.github.com>
2024-03-25 14:33:09 +0530

    Update config.py
    Update

--- config.py
+++ config.py
@@ -94,9 +94,9 @@ class Var(object):
     FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
     HAS_SSL=bool(getenv('HAS_SSL',False))
     if HAS_SSL:
-        URL = "https://hidden-annice-rngdebeloper.koyeb.app/"
+        URL = "https://vfilestore.onrender.com"
     else:
-        URL = "https://hidden-annice-rngdebeloper.koyeb.app/"
+        URL = "https://vfilestore.onrender.com"
 
 
 
