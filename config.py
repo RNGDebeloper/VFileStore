07f2bd6dfe3914b1c407babfc464a902f3e035b7
RNGDebeloper <131392755+RNGDebeloper@users.noreply.github.com>
2024-03-25 12:43:11 +0530

    Update config.py
    Updated

--- config.py
+++ config.py
@@ -94,9 +94,9 @@ class Var(object):
     FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
     HAS_SSL=bool(getenv('HAS_SSL',False))
     if HAS_SSL:
-        URL = ""
+        URL = "https://hidden-annice-rngdebeloper.koyeb.app/"
     else:
-        URL = ""
+        URL = "https://hidden-annice-rngdebeloper.koyeb.app/"
 
 
 
